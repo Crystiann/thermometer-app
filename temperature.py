39f4e31d3d1121ea771e921a34d72c8093072524
Cristian <crystian_marica@yahoo.com>
2019-11-16 12:04:08 +0200

    second commit

---
+++ temperature.py
@@ -0,0 +1,21 @@
+class termometru:
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    def get_temperature(self, unit = "C"):
+        if unit == "C":
+            return self.temperature
+        elif unit == "F":
+            return self.to_fahrenheit(self.temperature)
+        return self.temperature
+
+    def set_temperature(self, temperature):
+        self.temperature = temperature
+
+    def to_celsius(self, temperature):
+        temperature = (temperature - 32) * 5 / 9
+        return temperature
+
+    def to_fahrenheit(self, temperature):
+        temperature = temperature * 9 / 5 + 32
+        return temperature
